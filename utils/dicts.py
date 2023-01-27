69134c52ad24af1019500448b077169ddad64b92
Arina <sav2405@gmail.com>
2023-01-27 13:44:30 +0400

    add func get_val

--- utils/dicts.py
+++ utils/dicts.py
@@ -1,2 +1,12 @@
 def get_val(collection, key, default='git'):
-    pass
+    """
+    Функция возвращает значение из словаря по переданному ключу, если ключ существует.
+    В ином случае возвращается значение default
+    :param collection: исходный словарь
+    :param key: ключ
+    :param default: значение по умолчанию
+    :return:
+    """
+    if key in collection:
+        return collection[key]
+    return default
