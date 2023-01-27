fd6ec09d586ff5610ea000f909594925fd680f11
Arina <sav2405@gmail.com>
2023-01-27 19:51:17 +0400

    remove call decorators

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,25 @@
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture
 def test_dict():
     return {"vcs": "mercurial"}
 
 
-@pytest.fixture()
+@pytest.fixture
 def test_key():
     return "vcs"
 
-@pytest.fixture()
+@pytest.fixture
 def test_default_1():
     return "git"
 
 
-@pytest.fixture()
+@pytest.fixture
 def test_default_2():
     return "bazaar"
 
 
-@pytest.fixture()
+@pytest.fixture
 def test_value():
     return "mercurial"
